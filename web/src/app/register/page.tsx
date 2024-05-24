52c103907de97791c46cdce5f0c8736d33efc63a
Guilherme Orlandin <110955432+GuiOrlandin@users.noreply.github.com>
2024-05-24 10:39:31 -0300

    user register interaction with api implemented

--- web/src/app/register/page.tsx
+++ web/src/app/register/page.tsx
@@ -2,6 +2,8 @@
 
 import TopBar from "../components/topBar";
 
+import { useState, ChangeEvent } from "react";
+
 import { FcGoogle } from "react-icons/fc";
 import { signIn } from "next-auth/react";
 
@@ -16,8 +18,34 @@ import {
   RegisterPageContainer,
   RegisterWithGoogleButton,
 } from "./styles";
+import {
+  UserRegisterDetails,
+  useUserRegisterMutate,
+} from "@/hooks/createUserHook";
 
 export default async function Register() {
+  const { mutate } = useUserRegisterMutate();
+  const [userRegisterDetails, setUserRegisterDetails] =
+    useState<UserRegisterDetails>({
+      name: "",
+      password_hash: "",
+      email: "",
+    });
+
+  function handleChangeUserDetailsForRegister(
+    value: ChangeEvent<HTMLInputElement>,
+    inputTitle: string
+  ) {
+    setUserRegisterDetails({
+      ...userRegisterDetails!,
+      [inputTitle]: value,
+    });
+  }
+
+  function handleRegister(userRegisterDetails: UserRegisterDetails) {
+    mutate(userRegisterDetails);
+  }
+
   return (
     <RegisterPageContainer>
       <TopBar page="register" isLoged={false} />
@@ -25,18 +53,35 @@ export default async function Register() {
         <RegisterContent>
           <EmailInputContainer>
             <span>E-mail</span>
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(value) =>
+                handleChangeUserDetailsForRegister(value, "email")
+              }
+            />
           </EmailInputContainer>
           <NameInputContainer>
             <span>Name</span>
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(value) =>
+                handleChangeUserDetailsForRegister(value, "name")
+              }
+            />
           </NameInputContainer>
           <PasswordInputContainer>
             <span>Senha</span>
-            <input type="text" />
+            <input
+              type="text"
+              onChange={(value) =>
+                handleChangeUserDetailsForRegister(value, "password_hash")
+              }
+            />
           </PasswordInputContainer>
           <RegisterButtonContainer>
-            <RegisterButton>Cadastrar</RegisterButton>
+            <RegisterButton onClick={() => handleRegister(userRegisterDetails)}>
+              Cadastrar
+            </RegisterButton>
           </RegisterButtonContainer>
           <RegisterWithGoogleButton
             onClick={() => signIn("google", { callbackUrl: "/" })}
